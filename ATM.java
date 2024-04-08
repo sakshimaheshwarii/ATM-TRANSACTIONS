c081a0a01760fa80e3b1bc653b85c3f6e3856ce5
Sakshi Maheshwari <102533571+sakshimaheshwarii@users.noreply.github.com>
2024-04-09 03:12:19 +0530

    Update ATM.java

--- ATM.java
+++ ATM.java
@@ -3,23 +3,32 @@ import java.util.Scanner;
 class ATM {
     public static void main(String[] args) {
         int balance = 10000;
-        int deposit;
-        int debited;
         Scanner sc = new Scanner(System.in);
+
         while (true) {
             System.out.println("-----------ATM-------------");
             System.out.println("Choose 1 for Withdraw");
             System.out.println("Choose 2 for Deposit");
-            System.out.println("Choose 3 for Balance enquiry");
+            System.out.println("Choose 3 for Balance Enquiry");
             System.out.println("Choose 4 for Exit");
 
             System.out.print("Enter your Choice: ");
+            if (!sc.hasNextInt()) {
+                System.out.println("Invalid input. Please enter a number.");
+                sc.next(); // Consume the invalid input
+                continue;
+            }
             int num = sc.nextInt();
 
             switch (num) {
                 case 1:
                     System.out.print("Enter amount to be withdrawn: ");
-                    debited = sc.nextInt();
+                    if (!sc.hasNextInt()) {
+                        System.out.println("Invalid input. Please enter a number.");
+                        sc.next(); // Consume the invalid input
+                        continue;
+                    }
+                    int debited = sc.nextInt();
                     if (debited <= balance) {
                         balance -= debited;
                         System.out.println("Please collect the amount.");
@@ -31,21 +40,30 @@ class ATM {
 
                 case 2:
                     System.out.print("Enter amount to deposit: ");
-                    deposit = sc.nextInt();
+                    if (!sc.hasNextInt()) {
+                        System.out.println("Invalid input. Please enter a number.");
+                        sc.next(); // Consume the invalid input
+                        continue;
+                    }
+                    int deposit = sc.nextInt();
                     balance += deposit;
-                    System.out
-                            .println("Your money has been successfully deposited.\nYour current balance is:" + balance);
+                    System.out.println("Your money has been successfully deposited.");
+                    System.out.println("Your current balance is: " + balance);
                     System.out.println();
                     break;
 
                 case 3:
                     System.out.println("Let's proceed with the balance enquiry: ");
-                    System.out.println("Balance:" + balance);
+                    System.out.println("Balance: " + balance);
                     System.out.println();
                     break;
 
                 case 4:
                     System.exit(0);
+
+                default:
+                    System.out.println("Invalid choice. Please enter a valid option (1-4).");
+                    break;
             }
         }
     }
